51927290b59c5a4cdbde08117e66c8e852da0212
bryeone <brianjacob94@gmail.com>
2025-07-28 19:59:59 -0500

    Changed output to provide sorted list of characters

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from stats import get_word_count
 from stats import get_book_text
 from stats import get_book_dictionary
+from stats import sort_dict
+from stats import sort_on
 
 def main():
     text = get_book_text("books/frankenstein.txt")
@@ -8,7 +10,15 @@ def main():
     count = get_word_count("books/frankenstein.txt")
     #print(f"{count} words found in the document")
     dictionary = get_book_dictionary(text)
-    print(dictionary)
+    sorted_dictionary = sort_dict(dictionary)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for i in sorted_dictionary:
+        print(f"{i["char"]}: {i["num"]}")
+        #print(sorted_dictionary)
 
     
 
--- stats.py
+++ stats.py
@@ -10,15 +10,28 @@ def get_book_text(file_path):
     
 def get_book_dictionary(text):
     text = text.lower()
-    word_text = text.split()
+    #word_text = text.split()
     dictionary = {}
 
-    for word in word_text:
+    for word in text:
         if word not in dictionary:
             dictionary[word] = 1
         else:
             dictionary[word] = dictionary[word] + 1
     return dictionary
+
+def sort_on(item):
+    return item["num"]
+
+
+def sort_dict(dictionary):
+    sorted_dict = []
+    for i in dictionary:
+        if i.isalpha():
+            sorted_dict.append({"char": i, "num" : dictionary[i]})
     
-            
+    sorted_dict.sort(reverse=True, key=sort_on)
+    return sorted_dict
+
+
 
